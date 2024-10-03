c3e1bf0d7789d38449e882174f584b47f7a2a7de
roskamsh <breesh.roskamshieter@gmail.com>
2024-10-03 13:25:44 +0100

    update tests

--- test/test1.py
+++ test/test1.py
@@ -8,6 +8,9 @@ import os
 def generate_results():
     command = ["nextflow","run","main.nf","-c","test/test.config","-profile","ci"]
     result = subprocess.run(command, capture_output=True, text=True)
+    print(result.stdout)
+    if result.stderr:
+        print(result.stderr)
     return result
 
 # Check if the command was successful
@@ -16,8 +19,9 @@ def test_run_completed(generate_results):
 
 def test_ld_block_output(generate_results):
     ld_blocks = pd.read_csv("results/ld_blocks/snps_with_LD_blocks.csv")
-    assert len(ld_blocks.RSID)==3, "Expected 3 SNPs in output LD blocks file."
-    assert ld_blocks.columns == ['RSID','CHR','POS','LDblock_lower','LDblock_upper','LDblock_length','lower_bound','upper_bound'], "Column names do not match expected names in LD blocks file."
+    assert len(ld_blocks.RSID) == 3, "Expected 3 SNPs in output LD blocks file."
+    assert len(ld_blocks.columns) == 8, "Expected 8 columns in LD blocks file."
+    assert ld_blocks.columns.tolist() == ['RSID','CHR','POS','LDblock_lower','LDblock_upper','LDblock_length','lower_bound','upper_bound'], "Column names do not match expected in LD blocks file."
 
 def test_screeplot_exists(generate_results):
-    assert os.path.exists("results/pca/ScreePlot.pdf"), "Expected output ScreePlot from pipeline run."
+    assert os.path.exists("results/pca/ScreePlot.pdf"), "Expected file ScreePlot.pdf from pipeline run."
