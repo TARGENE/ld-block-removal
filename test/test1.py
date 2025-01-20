f191a5c991f11d935167e39c2046ce5117c3caee
roskamsh <breesh.roskamshieter@gmail.com>
2025-01-20 15:21:12 +0000

    correct syntax

--- test/test1.py
+++ test/test1.py
@@ -21,7 +21,7 @@ def test_ld_block_output(generate_results):
     ld_blocks = pd.read_csv("results/ld_blocks/snps_with_LD_blocks.csv", header=None)
     assert ld_blocks.shape[0] == 3, "Expected 3 SNPs in output LD blocks file."
     assert len(ld_blocks.columns) == 8, "Expected 8 columns in LD blocks file."
-    assert ld_blocks[ld_blocks.columns[2].isin([1,2,3])], "Expected chromosomes 1, 2 and 3 in output file."
+    assert ld_blocks[ld_blocks.columns[1]].isin([1,2,3]).all()==True, "Expected chromosomes 1, 2 and 3 in output file."
 
 def test_screeplot_exists(generate_results):
     assert os.path.exists("results/pca/ScreePlot.pdf"), "Expected file ScreePlot.pdf from pipeline run."
